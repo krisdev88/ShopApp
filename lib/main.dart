f428b735e94f56825e7279910f64282e54f8dbff
krisdev88 <kceleban09@gmail.com>
2023-02-25 08:36:24 +0100

    Add animation for routeNamed navigation

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import './screens/orders_screen.dart';
 import './screens/user_products_screen.dart';
 import './screens/edit_product_screen.dart';
 import './screens/auth_screen.dart';
+import './helpers/custom_route.dart';
 
 import 'providers/auth.dart';
 import 'providers/products.dart';
@@ -49,6 +50,10 @@ class MyApp extends StatelessWidget {
             fontFamily: 'Lato',
             colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
                 .copyWith(secondary: Colors.deepOrange),
+            pageTransitionsTheme: PageTransitionsTheme(builders: {
+              TargetPlatform.android: CustomPageTransitionBuilder(),
+              TargetPlatform.iOS: CustomPageTransitionBuilder(),
+            }),
           ),
           home: auth.isAuth
               ? ProductsOverviewScreen()
